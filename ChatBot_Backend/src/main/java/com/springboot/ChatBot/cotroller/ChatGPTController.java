dd5a4471fc480c8395364772bbac550e85849f4f
RAKESH M B <rakeshbhavimani1@gmail.com>
2025-04-02 16:29:19 +0530

    created frontend part of chatBot

--- ChatBot_Backend/src/main/java/com/springboot/ChatBot/cotroller/ChatGPTController.java
+++ ChatBot_Backend/src/main/java/com/springboot/ChatBot/cotroller/ChatGPTController.java
@@ -5,6 +5,7 @@ import com.springboot.ChatBot.service.ChatGPTSerivce;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("api/chat")
 public class ChatGPTController {
